48cf086b9f8a264fa2c0dcc1c28119ff75e57e59
Ahmed Soliman <ahmed.soliman@programmer.net>
2023-02-25 11:48:25 -0600

    added create article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to articles_path
+    else
+      render 'new', status: :bad_request
+    end
+  end
 end
