5b7b37415bf967757c7b7119a96ddd710768ae9a
Ahmed Soliman <ahmed@famous.co>
2023-02-27 11:22:25 -0600

    styled form errors

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# Disable form validation html feedback
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
