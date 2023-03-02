356d491aaa38e1d0e804f490230284144a0174ba
Ahmed Soliman <ahmed@famous.co>
2023-03-02 16:17:05 -0600

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :articles
 
-  validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
-  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+
+  EMAIL_REGEX = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/'
+  validates :email, presence: true, uniqueness: { case_sensitive: false },  length: { maximum: 105 }, format: { with: EMAIL_REGEX }
 end
