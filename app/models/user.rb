5d596ee78d4ffb4409d7bc6c0c7f634608ad2ec0
Ahmed Soliman <ahmed@famous.co>
2023-03-02 16:32:53 -0600

    debugged user email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,5 @@ class User < ApplicationRecord
   has_many :articles
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-
-  EMAIL_REGEX = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/'
-  validates :email, presence: true, uniqueness: { case_sensitive: false },  length: { maximum: 105 }, format: { with: EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },  length: { maximum: 105 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
