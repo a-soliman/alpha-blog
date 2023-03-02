4ab835eef2c1ba26e868286c4515f746ae61b698
Ahmed Soliman <ahmed@famous.co>
2023-03-02 16:51:18 -0600

    added backend user_password feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_221755) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_224640) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_221755) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
