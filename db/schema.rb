16a719cf70842869d46ac7ee626fc9e974e6e381
Ahmed Soliman <ahmed@famous.co>
2023-03-02 16:33:26 -0600

    created association between articles and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_220219) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_221755) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "uers", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
