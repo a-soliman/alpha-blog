a2d7f56cec6718efa016570d7e3fb50487a3b9e4
Ahmed Soliman <ahmed@famous.co>
2023-03-03 11:08:56 -0600

    user show functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80, class: "rounded-circle shodow" })
+    hash = Digest::MD5.hexdigest(user.email)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: options[:class])
+  end
 end
